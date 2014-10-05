7f93d07d137d8bc73c09ca37223a0efb43069521
Stefan Penner <stefan.penner@gmail.com>
2014-10-04 20:42:19 -0400

    update ember-cli

--- config/environment.js
+++ config/environment.js
@@ -2,11 +2,11 @@
 
 module.exports = function(environment) {
   var ENV = {
+    modulePrefix: 'perf',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
-      //MANDATORY_SETTER: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
@@ -20,15 +20,23 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    // LOG_MODULE_RESOLVER is needed for pre-1.6.0
-    // ENV.LOG_MODULE_RESOLVER = true;
-
     // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_MODULE_RESOLVER = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'auto';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
   }
 
   if (environment === 'production') {
