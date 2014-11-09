c0ee8fbe4d9394f7603eefd3600cc1e7aafb2f77
Stefan Penner <stefan.penner@gmail.com>
2014-11-09 18:56:54 -0500

    quick hack to see if the relationships are even eagerly consumed.
    Turns out they are not, if ED kept relationships lazy we could skip a lot of slow paths early.

--- app/models/equipment-item.js
+++ app/models/equipment-item.js
@@ -1,4 +1,5 @@
 import DS from "ember-data";
+import Ember from "ember";
 
 export default DS.Model.extend({
   qrId: DS.attr('number'),
@@ -6,8 +7,13 @@ export default DS.Model.extend({
   year: DS.attr('string'),
   size: DS.attr('string'),
   soleLength: DS.attr('number'),
-  type: DS.belongsTo('equipment_type'),
+  type: Ember.computed(function() {
+    return this.store.find('equipment_type', this.get('type'));
+  }),
   typeId: DS.attr('number'), // Duplicate of type - used so we don't have to query a relationship when putting the items into buckets by type for a much faster startup.
-  package: DS.belongsTo('package_level'),
-  currentRentals: DS.hasMany('rented_item')
+  //package: DS.belongsTo('package_level'),
+  package: Ember.computed('data', function() {
+    return this.store.find('package-level', this.get('data.package'));
+  }),
+  //currentRentals: DS.hasMany('rented_item')
 });
--- app/models/equipment-type.js
+++ app/models/equipment-type.js
@@ -1,8 +1,14 @@
 import DS from "ember-data";
+import Ember from "ember";
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   prefix: DS.attr('string'),
   hasPackage: DS.attr('boolean'),
-  items: DS.hasMany('equipment_item')
+  items: Ember.computed('data', function(){
+    var type = this;
+    return this.store.filter('equipment_item', function(item) {
+      return item.get('data.type') == type.get('id');
+    });
+  })
 });
